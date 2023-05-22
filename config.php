47b044b2250373e9bb8acd792183874cf9c05537
junaidch05 <99324029+junaidch05@users.noreply.github.com>
2023-05-22 12:23:33 +0500

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php 
 $server ="localhost";
-$username="id20787273_root";
-$password="Cd6785a1@";
-$database= "id20787273_scandiweb";
+$username="root";
+$password="";
+$database= "scandiweb_test";
 
 $conn = mysqli_connect($server,$username,$password,$database);
 if(!$conn){
